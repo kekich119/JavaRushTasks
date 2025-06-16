647384392184e27c004d18a2f3d79549a42e42bd
kekich119 <patowach3o@gmail.com>
2025-06-16 12:32:46 +0300

    Решил задачки

---
+++ src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print((Integer) 1);
+        print(2);
+    }
+
+    public static void print(Integer z) {
+        System.out.println(z);
+    }
+
+    public static void print(int v) {
+        System.out.println(v);
+    }
+
+}
