e0a9fda96321f862ea433bb39a5cc2f7a1ebe7d4
kekich119 <patowach3o@gmail.com>
2025-06-19 00:01:00 +0300

    задача

--- src/com/javarush/task/task16/task1625/Solution.java
+++ src/com/javarush/task/task16/task1625/Solution.java
@@ -11,6 +11,8 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         t1.start();
         t2.start();
+        t1.interrupt();
+        t2.interrupt();
     }
 
     public static class T1 extends Thread {
