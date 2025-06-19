117e51192dbd915e99dc56199dab5160c013491d
kekich119 <patowach3o@gmail.com>
2025-06-19 23:11:56 +0300

    Задачке

--- src/com/javarush/task/task16/task1629/Solution.java
+++ src/com/javarush/task/task16/task1629/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.task16.task1629;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Только по-очереди!
@@ -15,20 +17,36 @@ public class Solution {
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
 
-        //add your code here - добавьте код тут
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
 
         t1.printResult();
         t2.printResult();
     }
 
-    public static class Read3Strings extends Thread{
+    public static class Read3Strings extends Thread {
+        public List<String> list = new ArrayList<>();
+
         @Override
         public void run() {
             try {
-
                 String str = reader.readLine();
+                String str2 = reader.readLine();
+                String str3 = reader.readLine();
+                list.add(str);
+                list.add(str2);
+                list.add(str3);
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
+        }
+
+        public void printResult(){
+            for (String str : list){
+                System.out.print(str + " ");
             }
         }
     }
